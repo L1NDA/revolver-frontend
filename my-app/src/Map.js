15fae0be155f27a324dfba401ff009527b74d0f5
brodyzt <brodyzt@gmail.com>
2018-01-19 12:39:29 +0200

    Added new map

--- my-app/src/Map.js
+++ my-app/src/Map.js
@@ -1,5 +1,6 @@
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import React, {Component} from 'react' 
+var map_src = require('./map.png');
 
 export class MapContainer extends Component {
 
@@ -11,8 +12,15 @@ export class MapContainer extends Component {
       }
 
     render() {
+        const style = `
+            user-select: false;
+            
+        `
+
         return (
-            <Map google={window.google} width="80%" />
+            <div>
+                <img src={map_src} width="100%" height="100%" onclick="return false"/>
+            </div>
         );
     }
 }
