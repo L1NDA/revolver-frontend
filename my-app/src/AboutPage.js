3c9169b79221c88f4ac79fdf753eb1f8f5e5d377
Rachel Susanne Nash <rsn55@cornell.edu>
2018-01-19 10:09:14 +0200

    lsdfj

--- my-app/src/AboutPage.js
+++ my-app/src/AboutPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Logo from "./Logo.js";
+import './App.css';
 
 // var logo = require("./img/icon-small.png");
 
@@ -7,17 +8,19 @@ import Logo from "./Logo.js";
 class About extends Component {
   render() {
     return (
-      <div>
-      <Logo/>
+      <div className="aboutPage">
       <h3>ABOUT</h3>
       <p>Revolver is a data analytics service that uses deep learning to
       forecast global trends (e.g., rioting, crime, epidemics). Through social
        media mining, the Revolver team seeks to harness the power of AI and
        deep neural networks to gain hidden insights into big data on a global
        scale.</p>
+       <div className="closeDiv">
+       <button onClick={this.props.closePopup}>Close</button>
+       </div>
       </div>
     );
   }
 }
 
-export default Logo;
+export default About;
